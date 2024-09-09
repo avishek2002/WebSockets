d480b219f6b2a63e863d71e905bcd796a5b959a9
Avishek Sapkota Sharma <71004558+avishek2002@users.noreply.github.com>
2024-09-09 19:34:32 +1000

    added main functionalities for server and client program

--- Client.java
+++ Client.java
@@ -1,3 +1,11 @@
+/*
+ * Course : SENG4500 (Network and Distributed Computing)
+ * Programmer : Avishek
+ * Last Modified : 9/9/2024
+ * 
+ * This is the client program. It calls the server functions through the socket, 
+ * which are requested by the user through the cli.
+ */
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,16 +15,16 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-
-
 public class Client{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // the following should be configurable
-        String hostName = "localhost";
-        int portNumber = 8080;
+        // configurable hostname and port  number
+        System.out.print("Enter hostname : ");
+        String hostName = scanner.nextLine();
+        System.out.print("Enter TCP port to connect socket to : ");
+        int portNumber = scanner.nextInt();
 
         try (
             Socket socket = new Socket(hostName, portNumber);
@@ -25,8 +33,7 @@ public class Client{
             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
             
         ){
-            //do something here
-            System.out.println(in.readLine());
+            // main method to run the client cli
             run(out, in, stdIn, scanner);
         } catch (UnknownHostException e){
             System.err.println("Don't know about host " + hostName);
@@ -37,32 +44,26 @@ public class Client{
         }
     }
 
+    // method to run the respective function calls according to the user's choice
     public static void run(PrintWriter out, BufferedReader in, BufferedReader stdIn, Scanner scanner) throws IOException{
         int choice = 0;
         while (choice != -1) { 
             choice = printOptions(scanner);
             switch (choice) {
-                case 1:
-                    // calls method to ask user to enter a country and print the capital city of that country, which is retrieved from the server
+                case 1 -> // calls method to ask user to enter a country and print the capital city of that country, which is retrieved from the server
                     printCapital(out, in, stdIn, scanner);
-                    break;
-                case 2: 
-                    // calls a method to ask user to enter a country name and prints the estimated population retrieved from the server
+                case 2 -> // calls a method to ask user to enter a country name and prints the estimated population retrieved from the server
                     printPopulation(out, in, stdIn, scanner);
-                    break;
-                case 3:
-                    System.out.println("3");
-                    break;
-                case -1: 
-                    System.out.println("-1");
-                    break;
-                default:
-                    System.out.println("Selection out of range!");
+                case 3 -> // calls a method to ask user to enter country name and capital city; and adds it to the servers memory
+                    addCountryCapitalPair(out, in, stdIn, scanner);
+                case -1 -> System.exit(1);
+                default -> System.out.println("Selection out of range!");
             }
         }
     }
 
-    public static int  printOptions(Scanner scanner){
+    // method to print the options in the cli interface, and retrieve user's choice
+    public static int  printOptions(Scanner scanner) throws IOException{
         System.out.println("*".repeat(50));
         System.out.println("1 : Find capital city of country");
         System.out.println("2 : Find population of country");
@@ -74,6 +75,7 @@ public class Client{
         return choice;
     }
 
+    // method to ask server for the capital of a user specified country, if present in the server memory
     public static void printCapital(PrintWriter out, BufferedReader in, BufferedReader stdIn, Scanner scanner) throws IOException{
         out.println("1");
         System.out.print("Enter name of country to find its capital : ");
@@ -81,15 +83,52 @@ public class Client{
         String country = scanner.nextLine();
         out.println(country);
         String serverResponse = in.readLine();
-        System.out.println("Capital of " + country + " : " + serverResponse);
+        // country infomation not present in server memory
+        if (serverResponse.equals("403")){
+            System.out.println("Data for given country does not exist!");
+        }
+        // country information present in server memory
+        else{
+            System.out.println("Capital of " + country + " : " + serverResponse);
+        }
     }
 
+    // method to ask server to estimate the population of a user specified country, if present in the server memory
     public static void printPopulation(PrintWriter out, BufferedReader in, BufferedReader stdIn, Scanner scanner) throws IOException{
         out.println("2");
         System.out.print("Enter name of country to find its population : ");
         scanner.nextLine();
         String country = scanner.nextLine();
         out.println(country);
-        System.out.println("Popultion of " + country + "is estimated to be : " + in.readLine());
+        String serverResponse = in.readLine();
+        // country information not present in server memory; and hence population estimation not possible
+        if (serverResponse.equals("403")){
+            System.out.println("Data for given country does not exist!");
+        }
+        // successfully simulated population estimation
+        else{
+            System.out.println("Population of " + country + " is estimated to be : " + serverResponse);
+        }
+    }
+
+    // method to add country, capital pair on the server memory
+    public static void addCountryCapitalPair(PrintWriter out, BufferedReader in, BufferedReader stdIn, Scanner scanner) throws IOException{
+        out.println("3");
+        System.out.print("Enter name of country to add : ");
+        scanner.nextLine();
+        String country = scanner.nextLine();
+        System.out.print("Enter " + country + "'s capital : ");
+        String capital = scanner.nextLine();
+        // concatenating country and capital with a comma in the middle, so that one transmission is adequate
+        out.println(country + "," + capital);
+        String serverResponse = in.readLine();
+        // successfully added pair to the server memory
+        if ("201".equals(serverResponse)){
+            System.out.println("Successfully added country, capital pair to server memory!");
+        }
+        // unable to add pair to the server memory
+        else{
+            System.out.println("Country, capital pair already exists in the memory!");
+        }
     }
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,12 @@
+/*
+ * Course : SENG4500 (Network and Distributed Computing)
+ * Programmer : Avishek
+ * Last Modified : 9/9/2024
+ * 
+ * This is the server program. It stores country and capital pairs in a hashmap, 
+ * and performs functions requested by the client through the socket.
+ */
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -7,25 +16,27 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Server{
-    private static HashMap<String, String> capitals = new HashMap<String, String>();
+    // csv data stored in a hashmap (needs to be populated from the main method), inaccessible from outside this class
+    private static final HashMap<String, String> capitals = new HashMap<String, String>();
 
     public static void main(String[] args) {
-        // change this so its user configurable
-        int portNumber = 8080;
+        // user configurable tcp port
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the TCP port for the socket to listen on : ");
+        int portNumber = scanner.nextInt();
         populateCapitals();
 
         try (ServerSocket serverSocket = new ServerSocket(portNumber)){
-
             while (true) {
                 try (
                     Socket clientSocket = serverSocket.accept();
                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 ){
-                    // do something here
-                    out.println("lesgo");
+                    // main method for the server to call the client indicated function
                     runFunctions(out, in);
                 } catch (IOException e){
                     System.out.println("Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
@@ -36,50 +47,66 @@ public class Server{
         }
     }
 
+    // method to run methods requested from the client cli
     public static void runFunctions(PrintWriter out, BufferedReader in) throws IOException{
         String functionNumber = "";
         while (true) { 
             functionNumber = in.readLine();
             switch (functionNumber) {
-                case "1":
-                    String country = in.readLine();
-                    String capital = getCapital(country);
-                    out.println(capital);
-                    break;
-                case "2":
-                    out.println(getPopulation(in.readLine()));
-                    break;
-                case "3":
-                    addCapital();
-                    break;
-                default:
-                System.out.println(functionNumber);
+                case "1" -> out.println(getCapital(in.readLine()));
+                case "2" -> out.println(getPopulation(in.readLine()));
+                case "3" -> out.println(addCapital(in.readLine()));
+                default -> {
+                    System.out.println(functionNumber);
                     throw new AssertionError();
+                }
             }
         }
     }
 
+    // method to retrieve the capital city of a user specified country
     public static String getCapital(String country){
-        String capital = capitals.get(country);
+        // retrieve capital from the hashmap (note: key is stored in lower case for consistency purposes)
+        String capital = capitals.get(country.toLowerCase());
+        if (capital == null){
+            capital = "403";
+        }
         return capital;
     }
 
+    // method to calculate the population of a user specified country, the population is not accurate
     public static int getPopulation(String country){
+        // if country is not in our memory, we don't simulate the population estimation
+        if (!(capitals.containsKey(country.toLowerCase()))){
+            return 403; // forbidden request code
+        }
         Random random = new Random();
         return (random.nextInt(9) + 1) * 3393440;
     }
 
-    public static void addCapital(){
-
+    // method to add new country and capital pair to the memory (ie the hashmap)
+    public static int addCapital(String pair){
+        // split string to [country, capital] pair
+        String[] pairArray = pair.split(",");
+        // if the pair is  already in the memory
+        if ((capitals.containsKey(pairArray[0].toLowerCase()))){
+            return 403; // forbidden request; already exists
+        }
+        capitals.put(pairArray[0].toLowerCase(), pairArray[1]);
+        return 201; // successfully added resource
     }
 
+    // method to populate the hashmap storing country, capital pair
+    // make sure csv file is in the same directory as this program
     public static void populateCapitals(){
         String filename = "countries_capitals.csv";
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             String pair;
             while ((pair = reader.readLine()) != null) {
+                // split string to country, capital pair using String.split(regex)
                 String[] pairArray = pair.split(",");
-                capitals.put(pairArray[0], pairArray[1]);
+                // changing country value to lower case for consistency purposes
+                capitals.put(pairArray[0].toLowerCase(), pairArray[1]);
             }
         } catch (IOException e){
             System.out.println("Couldn't read file " + filename);
